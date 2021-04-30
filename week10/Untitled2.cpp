12016b830042bbcfec93655b7aaaee4c31a5ab87
lingdotdot <79676815+lingdotdot@users.noreply.github.com>
2021-04-30 10:55:28 +0800

    Add files via upload

---
+++ week10/Untitled2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+char line[1000];
+int main()
+{
+	int T;
+	scanf("%d\n\n", &T);
+
+	for(int t=0; t<T; t++){
+		int N=0;
+		while(get(line)=NULL ){
+			if(strcmp(line,"")==0)break;
+
+			N++;
+		}
+		printf("有幾棵樹? %d\n", N);
+		printf("=====分隔線=======\n");
+	}
+}
